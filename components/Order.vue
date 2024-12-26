dff3820b0a1849736535f87309beb3854c01a953
AS-Groop <asadbek.sheraliyev07@mail.ru>
2024-12-26 17:32:31 +0500

    full

---
+++ components/Order.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+    order: {
+        type: Array,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <div class="flex flex-col h-full justify-between">
+        <div>
+            <ul>
+                <li v-for="(pizza, index) in order" :key="index" class="mb-2 text-gray-900 dark:text-gray-100">
+                    {{ pizza.name }} - {{ pizza.price }} ₽ ({{ pizza.count }})
+                </li>
+            </ul>
+        </div>
+        <div class="text-gray-900 dark:text-gray-100 text-xl font-bold">
+            <div class="mb-2">
+                Итого: {{ total }} ₽
+            </div>
+        </div>
+    </div>
+</template>
